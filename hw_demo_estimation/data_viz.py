09f88430c422cee5b5a0687162ff273feef75857
Balint Mazzag <68548252+mbalint9901@users.noreply.github.com>
2022-12-19 01:48:31 +0100

    Add files via upload

--- hw_demo_estimation/data_viz.py
+++ hw_demo_estimation/data_viz.py
@@ -30,8 +30,8 @@ def plot_age_distribution_by_gender(nodes):
     sns.histplot(data=plot_df, x='AGE', hue='gender', bins=(np.arange(0, 45, 5) + 15))
 
 
-def plot_node_degree_by_gender(nodes, G):
-    """Plot the average of node degree across age and gender"""
+def data_node_degree_by_gender(nodes, G):
+    """Return the data needed for the plot of the average of node degree across age and gender"""
     nodes_w_degree = nodes.set_index('user_id').merge((pd.Series(dict(G.degree)).to_frame()),
                                                       how='left',
                                                       left_index=True,
@@ -39,12 +39,11 @@ def plot_node_degree_by_gender(nodes, G):
     nodes_w_degree = nodes_w_degree.rename({0: 'degree'}, axis=1)
     plot_df = nodes_w_degree.groupby(['AGE', 'gender']).agg({'degree': 'mean'}).reset_index()
     plot_df['Gender'] = plot_df['gender'].replace({0.0: 'woman', 1.0: 'man'})
-    fig = sns.lineplot(data=plot_df, x='AGE', y='degree', hue='Gender')
-    fig.set(xlabel='Age', ylabel='Degree')
+    return plot_df
 
 
-def plot_neighbour_connectivity_by_gender(nodes, G):
-    """Plot the neighbour connectivity of nodes across age and gender"""
+def data_neighbour_connectivity_by_gender(nodes, G):
+    """Return the data needed for the plot of the neighbour connectivity of nodes across age and gender"""
     neighbour_connectivity = nodes.set_index('user_id').merge(
         (pd.Series(networkx.average_neighbor_degree(G)).to_frame()),
         how='left',
@@ -53,12 +52,11 @@ def plot_neighbour_connectivity_by_gender(nodes, G):
     neighbour_connectivity = neighbour_connectivity.rename({0: 'neighbour_connect'}, axis=1)
     plot_df = neighbour_connectivity.groupby(['AGE', 'gender']).agg({'neighbour_connect': 'mean'}).reset_index()
     plot_df['Gender'] = plot_df['gender'].replace({0.0: 'woman', 1.0: 'man'})
-    fig = sns.lineplot(data=plot_df, x='AGE', y='neighbour_connect', hue='Gender')
-    fig.set(xlabel='Age', ylabel='Neighbour connectivity')
+    return plot_df
 
 
-def plot_clustering_coefficient_by_gender(nodes, G):
-    """Plot the neighbour connectivity of nodes across age and gender"""
+def data_clustering_coefficient_by_gender(nodes, G):
+    """Return the data needed for the plot of the neighbour connectivity of nodes across age and gender"""
     clustering_coefficient = nodes.set_index('user_id').merge((pd.Series(networkx.clustering(G)).to_frame()),
                                                               how='left',
                                                               left_index=True,
@@ -66,8 +64,21 @@ def plot_clustering_coefficient_by_gender(nodes, G):
     clustering_coefficient = clustering_coefficient.rename({0: 'cluster_coeff'}, axis=1)
     plot_df = clustering_coefficient.groupby(['AGE', 'gender']).agg({'cluster_coeff': 'mean'}).reset_index()
     plot_df['Gender'] = plot_df['gender'].replace({0.0: 'woman', 1.0: 'man'})
-    fig = sns.lineplot(data=plot_df, x='AGE', y='cluster_coeff', hue='Gender')
-    fig.set(xlabel='Age', ylabel='Clustering coefficient')
+    return plot_df
+
+
+def plot_figure_three(nodes, G):
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 6))
+    sns.lineplot(data=data_node_degree_by_gender(nodes, G), x='AGE', y='degree', hue='Gender',
+                 ax=ax1)
+    ax1.set(xlabel='Age', ylabel='Degree', title='(a) Degree centrality')
+    sns.lineplot(data=data_neighbour_connectivity_by_gender(nodes, G), x='AGE', y='neighbour_connect', hue='Gender',
+                 ax=ax2)
+    ax2.set(xlabel='Age', ylabel='Neighbour connectivity', title='(b) Neighbour connectivity')
+    sns.lineplot(data=data_clustering_coefficient_by_gender(nodes, G), x='AGE', y='cluster_coeff', hue='Gender',
+                 ax=ax3)
+    ax3.set(xlabel='Age', ylabel='Clustering coefficient', title='(c) Clustering coefficient')
+    plt.tight_layout()
 
 
 def plot_age_relations_heatmap(edges_w_features, gender_pair, normalize='logging'):
@@ -98,22 +109,30 @@ def plot_age_relations_heatmap(edges_w_features, gender_pair, normalize='logging
         plot_df_heatmap_norm = plot_df_heatmap.div(plot_df_heatmap.sum(axis=1), axis=0)
     else:
         plot_df_heatmap_norm = plot_df_heatmap
+    return plot_df_heatmap_norm
 
-    fig = sns.heatmap(plot_df_heatmap_norm)
-    fig.invert_yaxis()
 
-    if normalize == 'logging':
-        fig.set(title='Logged number of connections by pair')
-    elif normalize == 'rowsum':
-        fig.set(title='Proportion of connections by pair to total number of connections in the age group')
-    else:
-        fig.set(title='Number of connections by pair')
+def plot_figure_five(edges_w_features, normalize='logging'):
+    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2, figsize=(14, 10))
+    sns.heatmap(plot_age_relations_heatmap(edges_w_features=edges_w_features, gender_pair=0, normalize=normalize),
+                ax=ax1)
+    ax1.invert_yaxis()
+    ax1.set(xlabel="Age", ylabel="Age", title="(a) #connections per pair")
+    sns.heatmap(plot_age_relations_heatmap(edges_w_features=edges_w_features, gender_pair=1, normalize=normalize),
+                ax=ax2)
+    ax2.invert_yaxis()
+    ax2.set(xlabel="Age (Female)", ylabel="Age (Female)", title="(b) #connections per F-F pair")
+    sns.heatmap(plot_age_relations_heatmap(edges_w_features=edges_w_features, gender_pair=2, normalize=normalize),
+                ax=ax3)
+    ax3.invert_yaxis()
+    ax3.set(xlabel="Age (Male)", ylabel="Age (Male)", title="(c) #connections per M-M pair")
+    sns.heatmap(plot_age_relations_heatmap(edges_w_features=edges_w_features, gender_pair=3, normalize=normalize),
+                ax=ax4)
+    ax4.invert_yaxis()
+    ax4.set(xlabel="Age (Male)", ylabel="Age (Female)", title="(d) #connections per M-F pair")
 
-    if gender_pair == 0:
-        fig.set(xlabel='Age', ylabel='Age')
-    if gender_pair == 1:
-        fig.set(xlabel='Age (Female)', ylabel='Age (Female)')
-    if gender_pair == 2:
-        fig.set(xlabel='Age (Male)', ylabel='Age (Male)')
-    if gender_pair == 3:
-        fig.set(xlabel='Age (Female)', ylabel='Age (Male)')
+    if normalize == "rowsum":
+        plt.title('All values are proportionate to the sum of total connections in a given age group for X-axis')
+    if normalize == "logging":
+        plt.title('All values are log-scaled')
+    plt.tight_layout()
